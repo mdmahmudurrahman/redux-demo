53334ba0230a90ba39882766334cf69e6a613ccc
mdmahmudurrahman <md.mahmudur.rahman.bd@gmail.com>
2016-08-31 11:22:18 +0600

    initial commit and install npm

--- app/app.jsx
+++ app/app.jsx
@@ -3,3 +3,6 @@ var ReactDOM = require("react-dom");
 
 ReactDOM.render(<h1>boilerplate app!!</h1>,
  document.getElementById("app"));
+
+ require("./redux-example.jsx");
+ 
---
+++ spec.md
@@ -0,0 +1,1 @@
+01. Install redux => npm install 
