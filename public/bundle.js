7ed3e048636c3cdec484968a544d00fdf7d47a57
mdmahmudurrahman <md.mahmudur.rahman.bd@gmail.com>
2016-08-31 12:55:23 +0600

    Pure functions

--- public/bundle.js
+++ public/bundle.js
@@ -21414,9 +21414,108 @@
 
 	"use strict";
 
+	var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
 	var redux = __webpack_require__(173);
 
 	console.log("Starting redux example");
+
+	// pure functions
+
+	function add(a, b) {
+	  return a + b;
+	}
+
+	console.log(add(5, 6));
+
+	// The question: what it makes a pure function?
+	// => It's always going to return the same result giving the same
+	// input. If a is 1, b is 2, it always return 3.
+	//
+	// => There is no side effect. It does not rely on variables up above.
+	// meaning it does not updates/change any valiables outside of itself.
+	//
+	// => Our pure function can't have any asynchronous request, this means
+	// no promises or callbacks in the pure function.
+
+
+	var e = 3;
+
+	function add(b) {
+	  return e + b;
+	}
+
+	// **In this case our function is not pure. Because it relies on data
+	// outside of itself and giving the same input, we are not getting
+	// the same output, because the var a may be changed in the outside.
+
+	var result;
+	function add(a, b) {
+	  result = a + b;
+	  return result;
+	}
+
+	// ** Here this function is not also a pure function, because it
+	//    modifying something outside of it.
+
+	function add(a, b) {
+	  return a + b + new Date().getSeconds();
+	}
+
+	//    ** Here with the same input we are not always getting the same
+	//    output.
+	//
+	// ****** For the pure function, they are not allowed to update the data
+	// they passed in. This is really a matter for objects that are passed
+	// by reference, not by value.
+
+	// We can't do the following thing, because it change the actual object.
+
+	function changeProp(obj) {
+	  obj.name = "Mahmud";
+	  return obj;
+	}
+
+	var startingValue = {
+	  name: "Andrew",
+	  age: 25
+	};
+
+	var res = changeProp(startingValue);
+	console.log(startingValue);
+	console.log(res);
+	console.log(startingValue);
+
+	// We have to do the above thing in the following way(es6 syntax)
+	// ...obj => it actually grab the object and than return the changed value
+
+	function changeProp(obj) {
+	  console.log(obj.name);
+	  return _extends({}, obj, {
+	    name: "Mahmud"
+	  });
+	}
+
+	var startingValue = {
+	  name: "start",
+	  age: 26
+	};
+
+	var res = changeProp(startingValue);
+	console.log(startingValue);
+	console.log(res);
+	console.log(startingValue);
+
+	// This is very important for redux to create pure functions. Inside of your redux
+	// there are certain part of your code that have to be pure function, otherwise it will
+	// not work as expected.
+	// Three principles
+	//     a. Same output with the same input
+	//     b. There is no side effect. It does not rely on variables up above.
+	//        meaning it does not updates/change any valiables outside of itself.
+	//     c. Our pure function can't have any asynchronous request, this means
+	//        no promises or callbacks in the pure function.
+	//
 
 /***/ },
 /* 173 */
--- spec.md
+++ spec.md
@@ -1,1 +1,2 @@
-01. Install redux => npm install 
+01. Install redux => npm install react --save
+02.
