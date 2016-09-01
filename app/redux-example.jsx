0c3ab32db1d3d7cede259846831b12a1f48e27fb
mdmahmudurrahman <md.mahmudur.rahman.bd@gmail.com>
2016-09-01 10:39:47 +0600

    how to add arrays to action

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -1,178 +1,215 @@
-var redux = require("redux");
-
-console.log("Starting redux example");
-
-// pure functions
-
-function add (a,b) {
-  return a + b;
-}
-
-console.log(add(5,6));
-
-// The question: what it makes a pure function?
-// => It's always going to return the same result giving the same
-// input. If a is 1, b is 2, it always return 3.
+// var redux = require("redux");
 //
-// => There is no side effect. It does not rely on variables up above.
-// meaning it does not updates/change any valiables outside of itself.
+// console.log("Starting redux example");
 //
-// => Our pure function can't have any asynchronous request, this means
-// no promises or callbacks in the pure function.
-
-
-var e = 3;
-
-function add(b){
-  return e + b;
-}
-
-// **In this case our function is not pure. Because it relies on data
-// outside of itself and giving the same input, we are not getting
-// the same output, because the var a may be changed in the outside.
-
-var result;
-function add(a, b){
-  result = a + b;
-  return result;
-}
-
-// ** Here this function is not also a pure function, because it
-//    modifying something outside of it.
-
-   function add(a, b){
-     return a + b + new Date().getSeconds();
-   }
-
-//    ** Here with the same input we are not always getting the same
-//    output.
+// // pure functions
 //
-// ****** For the pure function, they are not allowed to update the data
-// they passed in. This is really a matter for objects that are passed
-// by reference, not by value.
-
-// We can't do the following thing, because it change the actual object.
-
-function changeProp(obj){
-  obj.name = "Mahmud";
-  return obj;
-}
-
-var startingValue = {
-  name: "Andrew",
-  age: 25
-};
-
-var res = changeProp(startingValue);
-console.log(startingValue);
-console.log(res);
-console.log(startingValue);
-
-// We have to do the above thing in the following way(es6 syntax)
-// ...obj => it actually grab the object and than return the changed value
-
-function changeProp(obj){
-  console.log(obj.name);
-  return {
-    ...obj,
-    name: "Mahmud"
-  };
-}
-
-
-var startingValue = {
-  name: "start",
-  age: 26
-};
-
-var res = changeProp(startingValue);
-console.log(startingValue);
-console.log(res);
-console.log(startingValue);
-
-
-// This is very important for redux to create pure functions. Inside of your redux
-// there are certain part of your code that have to be pure function, otherwise it will
-// not work as expected.
-// Three principles
-//     a. Same output with the same input
-//     b. There is no side effect. It does not rely on variables up above.
-//        meaning it does not updates/change any valiables outside of itself.
-//     c. Our pure function can't have any asynchronous request, this means
-//        no promises or callbacks in the pure function.
-
-
-// Creating reducer and getting app state: a reducer is basically a function.
-
-// var reducer = (state, action) => {
-//   state = state || {name: "Anonymous"}
-//   return state;
+// function add (a,b) {
+//   return a + b;
+// }
+//
+// console.log(add(5,6));
+//
+// // The question: what it makes a pure function?
+// // => It's always going to return the same result giving the same
+// // input. If a is 1, b is 2, it always return 3.
+// //
+// // => There is no side effect. It does not rely on variables up above.
+// // meaning it does not updates/change any valiables outside of itself.
+// //
+// // => Our pure function can't have any asynchronous request, this means
+// // no promises or callbacks in the pure function.
+//
+//
+// var e = 3;
+//
+// function add(b){
+//   return e + b;
+// }
+//
+// // **In this case our function is not pure. Because it relies on data
+// // outside of itself and giving the same input, we are not getting
+// // the same output, because the var a may be changed in the outside.
+//
+// var result;
+// function add(a, b){
+//   result = a + b;
+//   return result;
+// }
+//
+// // ** Here this function is not also a pure function, because it
+// //    modifying something outside of it.
+//
+//    function add(a, b){
+//      return a + b + new Date().getSeconds();
+//    }
+//
+// //    ** Here with the same input we are not always getting the same
+// //    output.
+// //
+// // ****** For the pure function, they are not allowed to update the data
+// // they passed in. This is really a matter for objects that are passed
+// // by reference, not by value.
+//
+// // We can't do the following thing, because it change the actual object.
+//
+// function changeProp(obj){
+//   obj.name = "Mahmud";
+//   return obj;
+// }
+//
+// var startingValue = {
+//   name: "Andrew",
+//   age: 25
 // };
 //
-// var store = redux.createStore(reducer);
-
-//or
-
-// var reducer = (state = {name: "Anonymous"}, action) => {
-//   // 1. here the reducer has a default state.
-//   // 2. our reducer function always returns a state. State is nothing but a
-//   // simple object with some properties
-//   return state;
+// var res = changeProp(startingValue);
+// console.log(startingValue);
+// console.log(res);
+// console.log(startingValue);
+//
+// // We have to do the above thing in the following way(es6 syntax)
+// // ...obj => it actually grab the object and than return the changed value
+//
+// function changeProp(obj){
+//   console.log(obj.name);
+//   return {
+//     ...obj,
+//     name: "Mahmud"
+//   };
+// }
+//
+//
+// var startingValue = {
+//   name: "start",
+//   age: 26
 // };
 //
-// var store = redux.createStore(reducer);
+// var res = changeProp(startingValue);
+// console.log(startingValue);
+// console.log(res);
+// console.log(startingValue);
 //
-// var currentState = store.getState()
-// console.log("currentState: ", currentState);
+//
+// // This is very important for redux to create pure functions. Inside of your redux
+// // there are certain part of your code that have to be pure function, otherwise it will
+// // not work as expected.
+// // Three principles
+// //     a. Same output with the same input
+// //     b. There is no side effect. It does not rely on variables up above.
+// //        meaning it does not updates/change any valiables outside of itself.
+// //     c. Our pure function can't have any asynchronous request, this means
+// //        no promises or callbacks in the pure function.
+//
+//
+// // Creating reducer and getting app state: a reducer is basically a function.
+//
+// // var reducer = (state, action) => {
+// //   state = state || {name: "Anonymous"}
+// //   return state;
+// // };
+// //
+// // var store = redux.createStore(reducer);
+//
+// //or
+//
+// // var reducer = (state = {name: "Anonymous"}, action) => {
+// //   // 1. here the reducer has a default state.
+// //   // 2. our reducer function always returns a state. State is nothing but a
+// //   // simple object with some properties
+// //   return state;
+// // };
+// //
+// // var store = redux.createStore(reducer);
+// //
+// // var currentState = store.getState()
+// // console.log("currentState: ", currentState);
+//
+//
 
+var redux = require('redux');
 
+console.log('Starting redux example');
 
-var reducer = (state = {name: "Anonymous"}, action) => {
+var stateDefault = {
+  name: "Anonymous",
+  hobbies: [],
+  movies: []
+}
+var nextHobbyId = 1;
+var nextMovieId = 1;
+
+var reducer = (state = stateDefault, action) => {
+  // state = state || {name: 'Anonymous'};
+
   switch (action.type) {
-    case "CHANGE_NAME":
+    case 'CHANGE_NAME':
       return {
         ...state,
         name: action.name
+      };
+    case "ADD_HOBBY":
+      return {
+        ...state,
+        hobbies: [
+          ...state.hobbies,
+          {
+            id: nextHobbyId++,
+            hobby: action.hobby
+          }
+        ]
+      };
+    case "ADD_MOVIE":
+      return {
+        ...state,
+        movies: [
+          ...state.movies,  // grabbing current element in the movies
+          {
+            id: nextMovieId++,
+            title: action.title,
+            genre: action.genre
+          }
+        ]
       };
     default:
       return state;
   }
 };
+var store = redux.createStore(reducer, redux.compose(
+  window.devToolsExtension ? window.devToolsExtension() : f => f
+));
 
-var store = redux.createStore(reducer);
-console.log("Current State: ", store.getState());
+// Subscribe to changes
+var unsubscribe = store.subscribe(() => {
+  var state = store.getState();
+
+  console.log('Name is', state.name);
+  console.log("New state:", store.getState());
+  document.getElementById('app').innerHTML = state.name;
+});
+// unsubscribe();
+
+store.dispatch({
+  type: 'CHANGE_NAME',
+  name: 'Andrew'
+});
 
 store.dispatch(
   {
-  type: "CHANGE_NAME",
-  name: "Andrew"
+    type: "ADD_HOBBY",
+    hobby: "running"
   }
 );
-console.log("Changed State: ", store.getState());
+store.dispatch({
+  type: 'CHANGE_NAME',
+  name: 'Emily'
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+store.dispatch({
+  type: "ADD_MOVIE",
+  title: "Mad Max",
+  genre: "Action"
+});
 
 
 
--- public/bundle.js
+++ public/bundle.js
@@ -55,8 +55,8 @@
 	  "boilerplate app!!"
 	), document.getElementById("app"));
 
-	// require("./redux-example.jsx");
 	__webpack_require__(172);
+	// require("./redux-todo-example.jsx");
 
 /***/ },
 /* 1 */
@@ -21413,58 +21413,219 @@
 /* 172 */
 /***/ function(module, exports, __webpack_require__) {
 
-	"use strict";
+	'use strict';
 
 	var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
 
+	function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
+
+	// var redux = require("redux");
+	//
+	// console.log("Starting redux example");
+	//
+	// // pure functions
+	//
+	// function add (a,b) {
+	//   return a + b;
+	// }
+	//
+	// console.log(add(5,6));
+	//
+	// // The question: what it makes a pure function?
+	// // => It's always going to return the same result giving the same
+	// // input. If a is 1, b is 2, it always return 3.
+	// //
+	// // => There is no side effect. It does not rely on variables up above.
+	// // meaning it does not updates/change any valiables outside of itself.
+	// //
+	// // => Our pure function can't have any asynchronous request, this means
+	// // no promises or callbacks in the pure function.
+	//
+	//
+	// var e = 3;
+	//
+	// function add(b){
+	//   return e + b;
+	// }
+	//
+	// // **In this case our function is not pure. Because it relies on data
+	// // outside of itself and giving the same input, we are not getting
+	// // the same output, because the var a may be changed in the outside.
+	//
+	// var result;
+	// function add(a, b){
+	//   result = a + b;
+	//   return result;
+	// }
+	//
+	// // ** Here this function is not also a pure function, because it
+	// //    modifying something outside of it.
+	//
+	//    function add(a, b){
+	//      return a + b + new Date().getSeconds();
+	//    }
+	//
+	// //    ** Here with the same input we are not always getting the same
+	// //    output.
+	// //
+	// // ****** For the pure function, they are not allowed to update the data
+	// // they passed in. This is really a matter for objects that are passed
+	// // by reference, not by value.
+	//
+	// // We can't do the following thing, because it change the actual object.
+	//
+	// function changeProp(obj){
+	//   obj.name = "Mahmud";
+	//   return obj;
+	// }
+	//
+	// var startingValue = {
+	//   name: "Andrew",
+	//   age: 25
+	// };
+	//
+	// var res = changeProp(startingValue);
+	// console.log(startingValue);
+	// console.log(res);
+	// console.log(startingValue);
+	//
+	// // We have to do the above thing in the following way(es6 syntax)
+	// // ...obj => it actually grab the object and than return the changed value
+	//
+	// function changeProp(obj){
+	//   console.log(obj.name);
+	//   return {
+	//     ...obj,
+	//     name: "Mahmud"
+	//   };
+	// }
+	//
+	//
+	// var startingValue = {
+	//   name: "start",
+	//   age: 26
+	// };
+	//
+	// var res = changeProp(startingValue);
+	// console.log(startingValue);
+	// console.log(res);
+	// console.log(startingValue);
+	//
+	//
+	// // This is very important for redux to create pure functions. Inside of your redux
+	// // there are certain part of your code that have to be pure function, otherwise it will
+	// // not work as expected.
+	// // Three principles
+	// //     a. Same output with the same input
+	// //     b. There is no side effect. It does not rely on variables up above.
+	// //        meaning it does not updates/change any valiables outside of itself.
+	// //     c. Our pure function can't have any asynchronous request, this means
+	// //        no promises or callbacks in the pure function.
+	//
+	//
+	// // Creating reducer and getting app state: a reducer is basically a function.
+	//
+	// // var reducer = (state, action) => {
+	// //   state = state || {name: "Anonymous"}
+	// //   return state;
+	// // };
+	// //
+	// // var store = redux.createStore(reducer);
+	//
+	// //or
+	//
+	// // var reducer = (state = {name: "Anonymous"}, action) => {
+	// //   // 1. here the reducer has a default state.
+	// //   // 2. our reducer function always returns a state. State is nothing but a
+	// //   // simple object with some properties
+	// //   return state;
+	// // };
+	// //
+	// // var store = redux.createStore(reducer);
+	// //
+	// // var currentState = store.getState()
+	// // console.log("currentState: ", currentState);
+	//
+	//
+
 	var redux = __webpack_require__(173);
 
-	// -searchText
-	// -showComplete
-	// -todos
+	console.log('Starting redux example');
 
-	console.log("Redux tutorial");
+	var stateDefault = {
+	  name: "Anonymous",
+	  hobbies: [],
+	  movies: []
+	};
+	var nextHobbyId = 1;
+	var nextMovieId = 1;
 
 	var reducer = function reducer() {
-	  var state = arguments.length <= 0 || arguments[0] === undefined ? { searchText: "", showComplete: false, todos: [] } : arguments[0];
+	  var state = arguments.length <= 0 || arguments[0] === undefined ? stateDefault : arguments[0];
 	  var action = arguments[1];
 
+	  // state = state || {name: 'Anonymous'};
+
 	  switch (action.type) {
-	    case "CHANGE_SEARCH_TEXT":
+	    case 'CHANGE_NAME':
 	      return _extends({}, state, {
-	        searchText: action.searchText
+	        name: action.name
+	      });
+	    case "ADD_HOBBY":
+	      return _extends({}, state, {
+	        hobbies: [].concat(_toConsumableArray(state.hobbies), [{
+	          id: nextHobbyId++,
+	          hobby: action.hobby
+	        }])
+	      });
+	    case "ADD_MOVIE":
+	      return _extends({}, state, {
+	        movies: [].concat(_toConsumableArray(state.movies), [// grabbing current element in the movies
+	        {
+	          id: nextMovieId++,
+	          title: action.title,
+	          genre: action.genre
+	        }])
 	      });
 	    default:
 	      return state;
 	  }
 	};
-
 	var store = redux.createStore(reducer, redux.compose(window.devToolsExtension ? window.devToolsExtension() : function (f) {
 	  return f;
 	}));
 
-	// Suscribe to changes
+	// Subscribe to changes
 	var unsubscribe = store.subscribe(function () {
 	  var state = store.getState();
-	  console.log("Current state", state);
+
+	  console.log('Name is', state.name);
+	  console.log("New state:", store.getState());
+	  document.getElementById('app').innerHTML = state.name;
+	});
+	// unsubscribe();
+
+	store.dispatch({
+	  type: 'CHANGE_NAME',
+	  name: 'Andrew'
 	});
 
 	store.dispatch({
-	  type: "CHANGE_SEARCH_TEXT",
-	  searchText: "text to search"
+	  type: "ADD_HOBBY",
+	  hobby: "running"
+	});
+	store.dispatch({
+	  type: 'CHANGE_NAME',
+	  name: 'Emily'
 	});
 
 	store.dispatch({
-	  type: "CHANGE_SEARCH_TEXT",
-	  searchText: "my text"
+	  type: "ADD_MOVIE",
+	  title: "Mad Max",
+	  genre: "Action"
 	});
 
-	unsubscribe();
-
-	store.dispatch({
-	  type: "CHANGE_SEARCH_TEXT",
-	  searchText: "Not shown text"
-	});
+	//
 
 /***/ },
 /* 173 */
